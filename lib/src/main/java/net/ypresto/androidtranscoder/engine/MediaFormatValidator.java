7afacd72b46108f78ab06add98d3120142635699
Manuel Rodríguez <dragermrb@users.noreply.github.com>
2020-03-11 13:24:57 +0100

    Update MediaFormatValidator.java

--- lib/src/main/java/net/ypresto/androidtranscoder/engine/MediaFormatValidator.java
+++ lib/src/main/java/net/ypresto/androidtranscoder/engine/MediaFormatValidator.java
@@ -33,7 +33,7 @@ class MediaFormatValidator {
     public static void validateAudioOutputFormat(MediaFormat format) {
         String mime = format.getString(MediaFormat.KEY_MIME);
         if (!MediaFormatExtraConstants.MIMETYPE_AUDIO_AAC.equals(mime)) {
-            throw new InvalidOutputFormatException("Audio codecs other than AAC is not supported, actual mime type: " + mime);
+            throw new InvalidOutputFormatException("Audio ---codecs other than AAC is not supported, actual mime type: " + mime);
         }
     }
 }
